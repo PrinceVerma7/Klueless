ec67a1e50de05d080a247e007b0ba4952e08677a
Monisha Rohilla <monisha.r12@gmail.com>
2018-10-29 14:40:15 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>yo</title>
+	<title>Klueless</title>
+	<style>
+#button {
+    width: 6em;
+    border: 2px solid green;
+    background: #ffe;
+    border-radius: 5px;
+}
+a {
+    display: block;
+    width: 100%;
+    line-height: 2em;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 5px;
+}
+a:hover {
+    color: red;
+    background: #eff;
+}
+</style>
 </head>
 <body>
-
+<div id="button"><a href="login.php">Login</a></div>
 </body>
 </html>
