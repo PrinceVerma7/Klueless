b2445acbe299893a7ced946386bb4004ee115fd3
Monisha Rohilla <monisha.r12@gmail.com>
2018-10-29 14:54:14 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,24 +2,8 @@
 <html>
 <head>
 	<title>Klueless</title>
-	<style>
-#button {
-    width: 6em;
-    border: 2px solid green;
-    background: #ffe;
-    border-radius: 5px;
-}
-a {
-    display: block;
-    width: 100%;
-    line-height: 2em;
-    text-align: center;
-    text-decoration: none;
-    border-radius: 5px;
-}
-</style>
 </head>
 <body>
-<div id="button"><a href="login.php">Login</a></div>
+<button><a href="login.php">Login</a></button>
 </body>
 </html>
