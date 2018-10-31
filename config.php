e2988c7d7198b1d55393b14e984b345c305a811b
Monisha Rohilla <monisha.r12@gmail.com>
2018-10-31 15:00:52 +0530

    .

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
 	$hostname_conn = "localhost:3306";
-	$database_conn = "Klueless41";
-	$username_conn = "klue41";
-	$password_conn = "12345";
+	$database_conn = "irisipiy_klueless";
+	$username_conn = "irisipiy_klue";
+	$password_conn = "!-6_3-_T4;Ja";
 	$db_prefix = "";
 	// $table_name="klueless13";
 	// $discussion_board_link="https://klueless13.wordpress.com";
 
-$conn = mysqli_connect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
+$conn = mysqli_connect($hostname_conn, $username_conn, $password_conn, $database_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysqli_select_db($conn, $database_conn) or DIE('Database name is not available!');
 ?>
+
+
